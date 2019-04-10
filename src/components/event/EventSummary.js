e8504ffdc4d9502e305052a8ed77ea461b2aea27
nico stran <nstranquist@gmail.com>
2019-04-10 11:22:13 -0500

    modularized eventlist, event summary

---
+++ src/components/event/EventSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const EventSummary = () => {
+    return(
+        <div className="card z-depth-0 event-summary">
+            <div className="card-content grey-text text-darken-3">
+                <span className="card-title">Project Title</span>
+                <p>Posted by venue owner</p>
+                <p className="grey-text">3 December, 3pm</p>
+            </div>
+        </div>
+    )
+}
+
+export default EventSummary;
