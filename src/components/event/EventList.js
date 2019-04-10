490f8726be7fd9f8326ceb85caa2fe95d19aacea
nico stran <nstranquist@gmail.com>
2019-04-09 20:51:23 -0500

    EventList.js, Notifications.js, <Switch> and <Route> routing within App.js

---
+++ src/components/event/EventList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const EventList = () => {
+    return (
+        <div className="event-list section">
+            <div className="card z-depth-0 event-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by venue owner</p>
+                    <p className="grey-text">3 December, 3pm</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 event-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by venue owner</p>
+                    <p className="grey-text">3 December, 3pm</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 event-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by venue owner</p>
+                    <p className="grey-text">3 December, 3pm</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default EventList;
