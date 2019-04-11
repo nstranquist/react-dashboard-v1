e577914e60152b83a47ecd05c3f2b925daa4eae8
nico stran <nstranquist@gmail.com>
2019-04-11 01:15:21 -0500

    added firestore to createEvent action in eventAction.js, began to access firestore data. pt1

--- src/store/actions/eventActions.js
+++ src/store/actions/eventActions.js
@@ -2,6 +2,18 @@ export const createEvent = (event) => {    //createEvent is the action creator
     return (dispatch, getState, { getFirebase, getFirestore }) => {
         //make async call to database
         //dispatch action again (just paused it)
-        dispatch({ type: 'CREATE_EVENT', event: event });
+        const firestore = getFirestore();
+        firestore.collection('events').add({
+            ...event,
+            hostname: 'testname',
+            hostposition: 'admin',
+            hostid: 12345,
+            dateCreated: new Date(),
+        }).then(() => {
+            dispatch({ type: 'CREATE_EVENT', event: event });
+        }).catch((err) => {
+            dispatch({ type: 'CREATE_EVENT_ERROR', err });
+        })
+        
     }
 };
