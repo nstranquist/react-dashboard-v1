372e367ce98948edf04acdd8c28d2b1ed28b9c1b
nico stran <nstranquist@gmail.com>
2019-04-10 21:22:19 -0500

    added background image, formatted it, index.css changes

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -4,9 +4,9 @@ import { NavLink } from 'react-router-dom';
 const SignedInLinks = () => {
     return (
         <ul className="right">
-            <li><NavLink to="/">New Event</NavLink></li>
-            <li><NavLink to="/">Log Out</NavLink></li>
-            <li><NavLink to="/" className="btn btn-floating pink lighten-1">NS</NavLink></li>
+            <li><NavLink to="/create">New Event</NavLink></li>
+            <li><NavLink to="/signout">Log Out</NavLink></li>
+            <li><NavLink exact to="/" className="btn btn-floating pink lighten-1">NS</NavLink></li>
         </ul>
     )
 }
