86bbbb1fe28c2192e8f15a38c5e0d5b1abffc249
nico stran <nstranquist@gmail.com>
2019-04-10 20:58:52 -0500

    added SignUp component, added <Route> in app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import EventDetails from './components/event/EventDetails';
 import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
             <Route exact path="/" component={Dashboard} />
             <Route path="/event/:id" component={EventDetails} />
             <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
           </Switch>
         </div>
       </BrowserRouter>
