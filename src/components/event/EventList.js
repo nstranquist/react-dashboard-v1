00aa970dff9914fb8396ba0163c49ef6c1aede6e
nico stran <nstranquist@gmail.com>
2019-04-28 00:15:36 -0500

    can click event and see event id now

--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import EventSummary from './EventSummary';
+import { Link } from 'react-router-dom';
 
 const EventList = ({events}) => {  //grabbing projects[] from props using js6 destructuring
     return (
         <div className="event-list section">
             { events && events.map(event => {  //includes "projects &&" portion because if no projects exist, don't bother mapping them
-                //we get 1 project each time through the array
+                //we get 1 event each time through the array
                 return (
-                    <EventSummary event={event} key={event.id} />
+                    <Link to={'/event/' + event.id}>
+                        <EventSummary event={event} key={event.id} />
+                    </Link>
                 )
             })}
         </div>
