dd90fe2ec542f137418bcd0447f1430605a495e0
nicostran <nstranquist@gmail.com>
2019-04-29 18:22:16 -0500

    wrote and deployed first cloud function to firebase

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,24 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
+// import and initialize admin from firebase
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config.firebase);
+
+/*exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!");
+});*/
+
+const createNotification = (notification) => {
+    return admin.firestore().collection('notifications').add(notification).then(doc => console.log('notification added', doc));
+}
+
+exports.eventCreated = functions.firestore.document('events/{eventId}').onCreate((doc) => {
+    const event = doc.data();
+    const notification = {
+        content: 'Added a new project',
+        user: `${event.firstName} ${event.lastName}`,
+        time: admin.firestore().FieldValue.serverTimestamp(),  /*stores whatever time at call of this function*/
+    }
+
+    return createNotification(notification);  /* calls above function using newly created 'notification' */
 });
