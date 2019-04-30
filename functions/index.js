2b42a504ba4f9805854e4464b17f6100743cee66
nicostran <nstranquist@gmail.com>
2019-04-30 16:52:08 -0500

    fixed cloud function bug

--- functions/index.js
+++ functions/index.js
@@ -2,23 +2,31 @@ const functions = require('firebase-functions');
 
 // import and initialize admin from firebase
 const admin = require('firebase-admin');
-admin.initializeApp(functions.config.firebase);
+/*const settings = {/* your settings...  timestampsInSnapshots: true};
+firestore.settings(settings);*/
+admin.initializeApp(functions.config.firebase); 
 
 /*exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!");
 });*/
 
 const createNotification = (notification) => {
-    return admin.firestore().collection('notifications').add(notification).then(doc => console.log('notification added', doc));
+    return admin.firestore().collection('notifications')
+        .add(notification)
+        .then(doc => console.log('notification added', doc));
 }
 
-exports.eventCreated = functions.firestore.document('events/{eventId}').onCreate((doc) => {
-    const event = doc.data();
-    const notification = {
-        content: 'Added a new project',
-        user: `${event.firstName} ${event.lastName}`,
-        time: admin.firestore().FieldValue.serverTimestamp(),  /*stores whatever time at call of this function*/
-    }
+exports.eventCreated = functions.firestore
+    .document('events/{eventId}')
+    .onCreate(doc => {
+    
+        const event = doc.data();
+        const notification = {
+            content: 'Added a new event',
+            user: `${event.firstName} ${event.lastName}`,
+            time: admin.firestore.FieldValue.serverTimestamp()  /*stores whatever time at call of this function*/
+        }
 
-    return createNotification(notification);  /* calls above function using newly created 'notification' */
+        return createNotification(notification);  /* calls above function using newly created 'notification' */
 });
+
